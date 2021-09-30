76b6cb9386b836e026cda336bf3406ef63152084
Chamara-Wijepala <mr.worf777@gmail.com>
2021-09-30 16:01:20 +0530

    Add functions to create and add properties to task divs

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -6,4 +6,28 @@ export function addTask() {
     let task = document.createElement('div');
     task.className = 'task';
     taskMenu.appendChild(task);
+
+    addTaskProperties(task);
+};
+
+function addTaskProperties(task) {
+    let propertyArray = getTaskProperties();
+
+    let title = document.createElement('p');
+    title.innerText = propertyArray[0];
+    task.appendChild(title);
+
+    let date = document.createElement('p');
+    date.innerText = propertyArray[1];
+    task.appendChild(date);
+
+    let priority = document.createElement('p');
+    priority.innerText = propertyArray[2];
+    task.appendChild(priority);
+};
+
+function getTaskProperties() {
+    let properties = Object.keys(createTask());
+
+    return properties;
 };
