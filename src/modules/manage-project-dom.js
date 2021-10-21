a07c36cbac7bffc726ade111a3c17a3b9dc55a08
Chamara-Wijepala <mr.worf777@gmail.com>
2021-10-21 14:48:25 +0530

    Add functions to select a project and store project id in localStorage

--- src/modules/manage-project-dom.js
+++ src/modules/manage-project-dom.js
@@ -1,4 +1,8 @@
-import { projectStorage, appendProject, setList } from "./manage-project-logic";
+import { projectStorage,
+         appendProject,
+         setList,
+         activeProject, 
+         selectProject } from "./manage-project-logic";
 
 const projectList = document.getElementById('project-list');
 const projectForm = document.getElementById('project-form');
@@ -9,30 +13,41 @@ export function renderList() {
     clearList();
     projectStorage.forEach(project => {
         const projectElement = document.createElement('li');
-        projectElement.className = 'project';
+        projectElement.classList.add('project');
         projectElement.dataset.id = project.id;
         projectElement.innerText = project.name;
+        if(project.id === activeProject) {
+            projectElement.classList.add('active-project');
+        };
         projectList.appendChild(projectElement);
     });
 };
-
 function clearList() {
     projectList.textContent = '';
 };
 
+//Creates a dynamically named project object and stores it in projectStorage
 projectForm.addEventListener('submit', e => {
     e.preventDefault();
     processForm();
     setList()
     renderList();
 });
-
 function processForm() {
     let projectName = projectInput.value;
     if (projectName === null || projectName === '') return;
     projectInput.value = null;
     appendProject(projectName);
 };
+
+//Applies a class to a clicked project then saves project's id in localStorage
+projectList.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'li') {
+        selectProject(e);
+        setList();
+        renderList();
+    };
+});
 
 /*
 export function removeProject() {
--- src/modules/manage-project-logic.js
+++ src/modules/manage-project-logic.js
@@ -1,8 +1,16 @@
 const LOCAL_STORAGE = 'project.list';
 export let projectStorage = JSON.parse(localStorage.getItem(LOCAL_STORAGE)) || [];
 
+const LOCAL_STORAGE_PROJECT_ID = 'project.projectId';
+export let activeProject = localStorage.getItem(LOCAL_STORAGE_PROJECT_ID);
+
 export function setList() {
     localStorage.setItem(LOCAL_STORAGE, JSON.stringify(projectStorage));
+    localStorage.setItem(LOCAL_STORAGE_PROJECT_ID, activeProject);
+};
+
+export function selectProject(e) {
+    activeProject = e.target.dataset.id;
 };
 
 function createProject(name) {
