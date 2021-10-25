f6845cdcadec869890c95d185d6541a130f32492
Chamara-Wijepala <mr.worf777@gmail.com>
2021-10-25 15:58:05 +0530

    Create function to dynamically create and add elements to task menu for each task in current project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
 import './styles/index.css';
-import { addTask, removeTask } from './modules/manage-task-dom';
 import { renderList } from './modules/manage-project-dom';
-import { setList } from './modules/manage-project-logic';
-
-addTask();
-addTask();
-
-removeTask();
 
 renderList();
--- src/modules/manage-task-dom.js
+++ src/modules/manage-task-dom.js
@@ -1,6 +1,21 @@
 import { createTask } from "./manage-task-logic";
+import { projectStorage, activeProject } from "./manage-project-logic";
 
 const taskMenu = document.getElementById('task-menu');
+const taskTemplate = document.getElementById('task-template');
+
+//Creates and appends task elements to taskMenu
+//for every task object in activeProject.tasks
+export function renderTaskList() {
+    const selectedProject = projectStorage.find(project => project.id === activeProject);
+    selectedProject.tasks.forEach(task => {
+        const taskElement = document.importNode(taskTemplate.content, true);
+        const taskName = taskElement.querySelector('p');
+        taskName.id = task.id;
+        taskName.innerText = task.name;
+        taskMenu.appendChild(taskElement);
+    });
+};
 
 export function addTask() {
     let task = document.createElement('div');
