6f7d05236b9d5be004ff1d6eb8718ead1a5eccfb
Chamara-Wijepala <mr.worf777@gmail.com>
2021-10-26 15:47:04 +0530

    Remove redundant code from manage-task-dom.js

--- src/modules/manage-task-dom.js
+++ src/modules/manage-task-dom.js
@@ -20,37 +20,3 @@ export function renderTaskList() {
 function clearTaskList() {
     taskMenu.textContent = '';
 };
-
-export function addTask() {
-    let task = document.createElement('div');
-    task.className = 'task';
-    taskMenu.appendChild(task);
-
-    addTaskProperties(task);
-};
-
-function addTaskProperties(task) {
-    let propertyArray = getTaskProperties();
-
-    let title = document.createElement('p');
-    title.innerText = propertyArray[0];
-    task.appendChild(title);
-
-    let date = document.createElement('p');
-    date.innerText = propertyArray[1];
-    task.appendChild(date);
-
-    let priority = document.createElement('p');
-    priority.innerText = propertyArray[2];
-    task.appendChild(priority);
-};
-
-function getTaskProperties() {
-    let properties = Object.keys(createTask());
-
-    return properties;
-};
-
-export function removeTask() {
-    taskMenu.removeChild(taskMenu.childNodes[0]);
-};
