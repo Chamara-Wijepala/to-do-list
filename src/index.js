995def1729472c95723d3db673517918498fe010
Chamara-Wijepala <mr.worf777@gmail.com>
2021-10-27 14:28:24 +0530

    Add function to run renderTaskList only if activeProjectId is not empty

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import './styles/index.css';
 import { renderList } from './modules/manage-project-dom';
+import { renderTaskList } from './modules/manage-task-dom';
+import { activeProjectId } from './modules/manage-project-logic';
 
-renderList();
+function loadPage() {
+    renderList();
+    if (activeProjectId !== 'null') {
+        renderTaskList();
+    }
+    else return;
+};
+
+loadPage();
